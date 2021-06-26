096bfb6ec6f47dd9cd16559c5a7e8b96b606c52a
Christy O'Reilly <christy@oreills.co.uk>
2021-06-26 19:57:33 +0100

    244 Move initialisation into loop

--- tinycode.py
+++ tinycode.py
@@ -1,7 +1,7 @@
 R=50
 S=set(range(R))
-b={c:1 for c in ((0,2),(1,3),(2,1),(2,2),(2,3))}
+b={}
 o=-1,1,0
 while 1:
- b={(x,y):int(2<sum((l:=b.get(((x+u)%R,(y+v)%R),0)) for u in o for v in o)-l/2<4) for x in S for y in S}
+ b={(x,y):int(2<sum((l:=b[(x+u)%R,(y+v)%R]) for u in o for v in o)-l/2<4 if b else (x,y) in {(0,2),(1,3),(2,1),(2,2),(2,3)}) for x in S for y in S}
  for y in S:print(''.join('.#'[b[x,y]] for x in S))
