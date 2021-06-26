8d8452ea6acc36b82acea76b8ac51bbd76b240b2
Christy O'Reilly <christy@oreills.co.uk>
2021-06-26 20:12:09 +0100

    238 Use arg unpacking so print joins strings by itself

--- tinycode.py
+++ tinycode.py
@@ -4,4 +4,4 @@ O=-1,1,0
 b={}
 while 1:
  b={(x,y):int(2<sum((l:=b[(x+u)%R,(y+v)%R]) for u in O for v in O)-l/2<4 if b else (x,y) in {(0,2),(1,3),(2,1),(2,2),(2,3)}) for x in S for y in S}
- for y in S:print(''.join('.#'[b[x,y]] for x in S))
+ for y in S:print(*['.#'[b[x,y]] for x in S])
