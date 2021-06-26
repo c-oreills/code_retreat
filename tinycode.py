8d1b52cda5065aa5f8ba8af981e7b249c5cf1546
Christy O'Reilly <christy@oreills.co.uk>
2021-06-26 15:14:56 +0100

    257 trim whitespace

--- tinycode.py
+++ tinycode.py
@@ -3,5 +3,5 @@ S=set(range(R))
 b={c:1 for c in ((0,2),(1,3),(2,1),(2,2),(2,3))}
 o=(-1,1,0)
 while 1:
- b = {(x,y):int(sum((l:=b.get(((x+u)%R,(y+v)%R),0)) for u in o for v in o)+10*l in (3,13,14)) for x in S for y in S}
- for y in S: print(''.join('.#'[b[x,y]] for x in S))
+ b={(x,y):int(sum((l:=b.get(((x+u)%R,(y+v)%R),0)) for u in o for v in o)+10*l in (3,13,14)) for x in S for y in S}
+ for y in S:print(''.join('.#'[b[x,y]] for x in S))
